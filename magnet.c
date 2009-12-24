2fa2711a9189d20c9268cd4ce1639ceee535fb81
| <sleepy.freaking.coder@gmail.com>
2009-12-24 21:38:00 +0000

    Code reformatting and use of EXIT_FAILURE/EXIT_SUCCESS.

--- magnet.c
+++ magnet.c
@@ -12,63 +12,62 @@ static int
 magnet_print(lua_State *L)
 {
 	const char *s = luaL_checkstring(L, 1);
-	printf("%s", s);
-	return 0;
+	fputs(s, stdout);
+	return EXIT_SUCCESS;
 }
 
-static int magnet_cache_script(lua_State *L, const char *fn, time_t mtime) {
-	/* 
-	 * if func = loadfile("<script>") then return -1
-	 *
-	 * magnet.cache["<script>"].script = func
-	 */
-
-	if (luaL_loadfile(L, fn)) {
+static int
+magnet_cache_script(lua_State *L, const char *fn, time_t mtime)
+{
+	/* Compile it as a chunk, push it as a function onto the Lua stack. */
+	if (luaL_loadfile(L, fn))
+	{
 		fprintf(stderr, "%s\n", lua_tostring(L, -1));
 		lua_pop(L, 1); /* remove the error-msg */
-
-		return -1;
+		return EXIT_FAILURE;
 	}
 
-	lua_getfield(L, LUA_GLOBALSINDEX, "magnet"); 
+	/* Push _G.magnet and _G.magnet.cache onto the Lua stack. */
+	lua_getfield(L, LUA_GLOBALSINDEX, "magnet");
 	lua_getfield(L, -1, "cache");
 
-	/* .script = <func> */
+	/* <table>.script = <func> */
 	lua_newtable(L); 
 	assert(lua_isfunction(L, -4));
 	lua_pushvalue(L, -4); 
-	lua_setfield(L, -2, "script");
+	lua_setfield(L, -2, "script"); /* Pops the function. */
 
+	/* <table>.mtime = <mtime> */
 	lua_pushinteger(L, mtime);
-	lua_setfield(L, -2, "mtime");
+	lua_setfield(L, -2, "mtime");  /* Pops the mtime integer. */
 
+	/* <table>.hits = <hits> */
 	lua_pushinteger(L, 0);
-	lua_setfield(L, -2, "hits");
+	lua_setfield(L, -2, "hits");   /* Pops the hits integer. */
 
-	/* magnet.cache["<script>"] = { script = <func> } */
-	lua_setfield(L, -2, fn);
+	/* magnet.cache['<script>'] = <table> */
+	lua_setfield(L, -2, fn);       /* Pops the "anonymous" table. */
 
-	lua_pop(L, 2); /* pop magnet and magnet.cache */
+	lua_pop(L, 2); /* Pop _G.magnet and _G.magnet.cache */
 
-	/* on the stack should be the function itself */
+	/* On the stack should be the function itself. */
 	assert(lua_isfunction(L, lua_gettop(L)));
 
 	return 0;
 }
 
-static int magnet_get_script(lua_State *L, const char *fn) {
+static int
+magnet_get_script(lua_State *L, const char *fn)
+{
 	struct stat st;
 	time_t mtime = 0;
 
 	assert(lua_gettop(L) == 0);
 
-	if (-1 == stat(fn, &st)) {
-		return -1;
-	}
+	if (stat(fn, &st) == -1)
+		return EXIT_FAILURE;
 
 	mtime = st.st_mtime;
-
-	/* if not magnet.cache["<script>"] then */
 
 	lua_getfield(L, LUA_GLOBALSINDEX, "magnet"); 
 	assert(lua_istable(L, -1));
@@ -77,51 +76,57 @@ static int magnet_get_script(lua_State *L, const char *fn) {
 
 	lua_getfield(L, -1, fn);
 
-	if (!lua_istable(L, -1)) {
-		lua_pop(L, 3); /* pop the nil */
-
-		if (magnet_cache_script(L, fn, mtime)) {
-			return -1;
-		}
-	} else {
+	/* magnet.cache['<script>'] is not a table for some reason, re-cache. */
+	if (!lua_istable(L, -1))
+	{
+		lua_pop(L, 3); /* Pop the nil. */
+		if (magnet_cache_script(L, fn, mtime))
+			return EXIT_FAILURE;
+	}
+	else
+	{
 		lua_getfield(L, -1, "mtime");
 		assert(lua_isnumber(L, -1));
 
-		if (mtime == lua_tointeger(L, -1)) {
+		/* Script has not been modified, continue as usual. */
+		if (mtime == lua_tointeger(L, -1))
+		{
 			lua_Integer hits;
-
 			lua_pop(L, 1);
 	
-			/* increment the hit counter */	
+			/* Increment the hit counter. */	
 			lua_getfield(L, -1, "hits");
 			hits = lua_tointeger(L, -1);
 			lua_pop(L, 1);
 			lua_pushinteger(L, hits + 1);
 			lua_setfield(L, -2, "hits");
 
-			/* it is in the cache */
+			/* It is in the cache. */
 			assert(lua_istable(L, -1));
 			lua_getfield(L, -1, "script");
 			assert(lua_isfunction(L, -1));
 			lua_insert(L, -4);
 			lua_pop(L, 3);
 			assert(lua_isfunction(L, -1));
-		} else {
-			lua_pop(L, 1 + 3);
-
-			if (magnet_cache_script(L, fn, mtime)) {
-				return -1;
-			}
 		}
-
-		/* this should be the function */
+		/* Recorded magnet.cache['<script>'].mtime does
+		** not match the actual mtime, re-cache. */
+		else
+		{
+			lua_pop(L, 4);
+			if (magnet_cache_script(L, fn, mtime))
+				return EXIT_FAILURE;
+		}
 	}
+	/* This should be the function (top of Lua stack). */
 	assert(lua_gettop(L) == 1);
 
-	return 0;
+	return EXIT_SUCCESS;
 }
 
-int main(int argc, char **argv) {
+int
+main(void)
+{
 	lua_State *L; 
 
 	L = luaL_newstate();
@@ -132,50 +137,58 @@ int main(int argc, char **argv) {
 	lua_setfield(L, -2, "cache");
 	lua_setfield(L, LUA_GLOBALSINDEX, "magnet");
 
-	while (FCGI_Accept() >= 0) {
+	while (FCGI_Accept() >= 0)
+	{
 		assert(lua_gettop(L) == 0);
 
-		if (magnet_get_script(L, getenv("SCRIPT_FILENAME"))) {
-			printf("Status: 404\r\n\r\n");
-
+		if (magnet_get_script(L, getenv("SCRIPT_FILENAME")))
+		{
+			fputs("Status: 404\r\n\r\n", stdout);
 			assert(lua_gettop(L) == 0);
 			continue;
 		}
 		/**
-		 * we want to create empty environment for our script 
+		 * We want to create empty environment for our script. 
 		 * 
 		 * setmetatable({}, {__index = _G})
 		 * 
-		 * if a function, symbol is not defined in our env, __index will lookup 
-		 * in the global env.
+		 * If a function symbol is not defined in our env,
+		 * __index will look it up in the global env. 
 		 *
-		 * all variables created in the script-env will be thrown 
-		 * away at the end of the script run.
-		 */
-		lua_newtable(L); /* my empty environment */
+		 * All variables created in the script-env will be thrown 
+		 * away at the end of the script run. */
+
+		/* Empty environment; will become parent to _G._G */
+		lua_newtable(L);
 
 		/* we have to overwrite the print function */
 		lua_pushcfunction(L, magnet_print);                       /* (sp += 1) */
 		lua_setfield(L, -2, "print"); /* -1 is the env we want to set(sp -= 1) */
 
-		lua_newtable(L); /* the meta-table for the new env  */
+		lua_newtable(L); /* The meta-table for the new env.  */
 		lua_pushvalue(L, LUA_GLOBALSINDEX);
-		lua_setfield(L, -2, "__index"); /* { __index = _G } */
-		lua_setmetatable(L, -2); /* setmetatable({}, {__index = _G}) */
-		lua_setfenv(L, -2); /* on the stack should be a modified env */
+		lua_setfield(L, -2, "__index"); /* { __index = _G }                         */
+		lua_setmetatable(L, -2);        /* setmetatable({}, { __index = _G })       */
+		lua_setfenv(L, -2);             /* On the stack should be the modified env. */
 
-		if (lua_pcall(L, 0, 1, 0)) {
-			fprintf(stderr, "%s\n", lua_tostring(L, -1));
-			lua_pop(L, 1); /* remove the error-msg and the function copy from the stack */
+		if (lua_pcall(L, 0, 1, 0))
+		{
+			/* Unsure about this. */
+			/* fprintf(stderr, "%s\n", lua_tostring(L, -1)); */
+			fputs("Status: 503\r\n\r\n", stdout);
+			fputs(lua_tostring(L, -1)  , stdout);
+
+			/* Remove the error message. */
+			lua_pop(L, 1);
 
 			continue;
 		}
 
+		/* Remove the function copy from the stack. */
 		lua_pop(L, 1);
 		assert(lua_gettop(L) == 0);
 	}
 
 	lua_close(L);
-
-	return 0;
+	return EXIT_SUCCESS;
 }	
